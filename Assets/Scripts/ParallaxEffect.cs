d58396a27424b9cedea81f296b74a03f63ed7790
NealLyonsWake <waken.games@gmail.com>
2023-04-08 02:56:23 +0100

    Adjustment of environment art assets and completion of inital parallax effect for scrolling.

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Parallex effect for the environment background and sky
+/// Cite: Unity Parallax Tutorial - How to infinite scrolling background, Dani:
+/// https://www.youtube.com/watch?v=zit45k6CUMk
+/// </summary>
+public class ParallaxEffect : MonoBehaviour
+{
+    // Deterine the length and start position of the environment sprites
+    private float length;
+    private float startPos;
+
+    // Get the camera
+    private Transform cam;
+
+    // Variable on how much parallax to apply
+    public float parallax;
+
+
+    void Start()
+    {
+        // Init the cam
+        cam = Camera.main.transform;
+
+        // Set the start position of the environment sprite
+        startPos = transform.position.x;
+
+        // Set the length of the environment sprite using the sprite renderer component
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    void Update()
+    {
+        // Record how far the camera has moved
+        float temp = (cam.position.x * (1-parallax));
+
+        // Make a temporary variable that measures the distance from the start point
+        float dist = (cam.position.x * parallax);
+
+        // Change the position reletive to cam position, start position and parallax level
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        // Adjust the start position pending left or right away from the camera
+        if (temp > startPos + length) startPos += (length*2);
+        else if (temp < startPos - length) startPos -= (length*2);
+    }
+}
